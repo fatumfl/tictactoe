c0d47393912ab203574b26b9730036836afa9909
Lenar M. Faskhutdinov <fatum.fl@gmail.com>
2016-07-04 17:39:27 +0300

    Small improvements

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # Tic-Tac-Toe by FatumFL
-# v0.1.1
+# v0.1.2
 
-from random import choice, randint
+from random import choice
 
 class Board(object):
 	# Gameboard
@@ -57,9 +57,8 @@ class Board(object):
 	def store_board(self, winner):
 		# Writes game result to a file. Winner should be either 0 or 1.
 		if self.board[0]:
-			db = open("rounds.text", mode="a")
-			print(self.board, '\t', winner, file=db)
-			db.close()
+			with open("rounds.text", mode="a") as db:
+				print(self.board, '\t', winner, file=db)
 			return True
 		else:
 			return False
@@ -71,8 +70,8 @@ class AI(object):
 		self.lvl = 1
 	
 	def _easy(self, empty_cells):
-		move = choice(empty_cells)
-		return move
+		cell = choice(empty_cells)
+		return cell
 
 	def _medium(self, board):
 		pass
@@ -88,25 +87,8 @@ class AI(object):
 		if self.lvl == 3:
 			return self._hard(data)
 
-
-
-
-
-
-
-
-
-
-
-
-player = '' # Stores mark of the player
-comp = '' # Stores mark of the computer
-turn = 0 # Defines the order: 0 - player turn, 1 - comp turn
-
 def greeting():
 	# Defines who goes first and prints greeting.
-	from random import choice, radint
-	
 	player = choice(['X', 'O'])
 	if player == 'X':
 		print("Hello, Mr. X! Make the first move.")
@@ -118,86 +100,32 @@ def greeting():
 		next_one = 1
 	return player, comp, next_one
 
-def players_move():
-	global board
-	row, column = map(int, input("Please enter number of the cell as follows ROWxCOLUMN: ").split('x'))
-	if column > 3 or column < 1 or row > 3 or row < 1:
-		print("There is no such column or/and row number. Max is 3, min is 1.")
-		players_move()
-	else:
-		cell = board[row - 1][column - 1]
-		if cell is not None:
-			print("There is already {} in this cell.".format(cell))
-			players_move()
-		else:
-			board[row - 1][column - 1] = player
-	return 0
+def players_move(empty_cells):
+	cell = int(input("Please enter number of the cell [1-9]:"))
+	if cell > 9 or cell < 1:
+		print("There is no such cell number. Max is 9, min is 1.")
+		players_move(empty_cells)
+	elif cell not in empty_cells:
+			print("There is already mark in this cell.")
+			players_move(empty_cells)
+	return cell
 
-def comps_move():
-	from random import randint
-	global board
-	row, column = randint(0,2), randint(0,2)
-	cell = board[row][column]
-	if cell is not None:
-		comps_move()
-	else:
-		print("Let me think... My move will be:")
-		board[row][column] = comp
-	return 1
-
-def check_board():
-	# Checking rows:
-	if board[0][0]==board[0][1]==board[0][2]!=None:
-		print("Game over! Mr. {} wins!".format(board[0][0]))
-		return 1
-	elif board[1][0]==board[1][1]==board[1][2]!=None:
-		print("Game over! Mr. {} wins!".format(board[1][0]))
-		return 1
-	elif board[2][0]==board[2][1]==board[2][2]!=None:
-		print("Game over! Mr. {} wins!".format(board[2][0]))
-		return 1
-	elif board[0][0]==board[1][0]==board[2][0]!=None:
-		print("Game over! Mr. {} wins!".format(board[0][0]))
-		return 1
-	elif board[0][1]==board[1][1]==board[2][1]!=None:
-		print("Game over! Mr. {} wins!".format(board[0][1]))
-		return 1
-	elif board[0][2]==board[1][2]==board[2][2]!=None:
-		print("Game over! Mr. {} wins!".format(board[0][2]))
-		return 1
-	elif board[0][0]==board[1][1]==board[2][2]!=None:
-		print("Game over! Mr. {} wins!".format(board[0][0]))
-		return 1
-	elif board[0][2]==board[1][1]==board[2][0]!=None:
-		print("Game over! Mr. {} wins!".format(board[0][2]))
-		return 1
-	else:
-		trigger = 0
-		for row in board:
-			if None in row:
-				trigger = 1
-		if trigger == 0:
-				print("Game over! It's a tie!")
-				return 1
-	return 0
-
-def print_board():
-	for row in board:
-		for i in row:
-			if i is None:
-				print('_', end=' ')
-			else:
-				print(i, end=' ')
-		print()
-
-if __name__ == "__main__":
+def main():
+	gameboard = Board()
+	comp_ai = AI()
 	player, comp, turn = greeting()
 	
-	while check_board() != 1:
+	while gameboard.check_board() == 0:
+		empty_cells = gameboard.list_empty_cells()
 		if turn == 0:
-			players_move()
+			cell = players_move(empty_cells)
+			gameboard.add_mark(cell, player)
 			turn = 1
 		else:
-			comps_move()
+			cell = comp_ai.ai_turn(empty_cells)
+			gameboard.add_mark(cell, comp)
 			turn = 0
-		print_board()
+		gameboard.print_board()
+
+if __name__ == "__main__":
+	main()
